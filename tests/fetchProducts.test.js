382ad13b37d6248246a5d5eee3df7877c9293b3b
Renan Storrer <storrerrenan@gmail.com>
2022-08-09 15:49:43 -0300

    requisito 1 - deu erro

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,12 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  test('Verifica se o fetchProducts é uma função', () => {
+    expect(typeof fetchProducts).toEqual('function')
+  })
+  
+  it('Testa se o fetch foi chamado, ao executar a função com o parametro "computador"', async () => {
+     await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  });
 });
