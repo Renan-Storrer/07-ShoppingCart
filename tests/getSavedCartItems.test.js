f47d17b97e104c2ed399bbc1f300f47494b95721
Renan Storrer <storrerrenan@gmail.com>
2022-08-12 01:17:10 -0300

    requisito 7 - correto ?

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -4,6 +4,17 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  
+  test('Testa se ao executar a funão, o metodo local storage é chamado!;', () => {
+    const parametro = 'cartItems';
+    const função = getSavedCartItems(parametro);
+    expect(localStorage.getItem).toHaveBeenCalled();
+  })
+
+  test('Testa se ao executar a gunção, o método local storage é chamado com o parametro "cartItem"', () => {
+    const parametro = 'cartItems';
+    const função = getSavedCartItems(parametro);
+    expect(localStorage.getItem).toHaveBeenCalledWith(parametro);
+  })
+
 });
