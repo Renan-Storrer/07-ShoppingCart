7ac723ba233d061db806e53e11934a8f6bce593d
Renan Storrer <storrerrenan@gmail.com>
2022-08-12 01:24:21 -0300

    requisito 6

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,6 +4,15 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+
+  test('Testa se, ao executar a função com o parametro "<ol><li>Item</li></ol>", o método "localStorage.setItem" é chamado', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalled();
+  })
+
+  test('Testa se, ao executar a função com o parametro "<ol><li>Item</li></ol>", o método localStorage.setItem é chamado com os parametros corretos', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', '<ol><li>Item</li></ol>');
+  })
+
 });
